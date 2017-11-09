bc457141acd939e04463180a2bb81222ea3b5b16
Rejoyline <31914628+Rejoyline@users.noreply.github.com>
2017-11-09 09:32:21 +0530

    first commit

---
+++ WalinnsMobileAnalytics/WalinnsMobileAnalytics.h
@@ -0,0 +1,19 @@
+//
+//  WalinnsMobileAnalytics.h
+//  WalinnsMobileAnalytics
+//
+//  Created by WALINNS INNOVATION on 08/11/17.
+//  Copyright © 2017 WALINNS INNOVATION. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WalinnsMobileAnalytics.
+FOUNDATION_EXPORT double WalinnsMobileAnalyticsVersionNumber;
+
+//! Project version string for WalinnsMobileAnalytics.
+FOUNDATION_EXPORT const unsigned char WalinnsMobileAnalyticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WalinnsMobileAnalytics/PublicHeader.h>
+
+
---
+++ WalinnsMobileAnalytics/WalinnsTracker.swift
@@ -0,0 +1,16 @@
+//
+//  WalinnsTracker.swift
+//  WalinnsMobileAnalytics
+//
+//  Created by WALINNS INNOVATION on 08/11/17.
+//  Copyright © 2017 WALINNS INNOVATION. All rights reserved.
+//
+
+import UIKit
+
+class WalinnsTracker: NSObject {
+
+    open class func hello(){
+        debugPrint("Hello from AlamoWater!")  //git@github.com:Rejoyline/WalinnsTracker.git
+    }
+}
